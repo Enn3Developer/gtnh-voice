78b154b205dfe89b8aab7f40f109f751a6d56566
Enn3Developer <arturpeshko39@gmail.com>
2026-07-06 20:28:43 +0200

    gitignored code review + added jacoco for test coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,18 @@
 
 plugins {
     id("com.gtnewhorizons.gtnhconvention")
+    jacoco
 }
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
 }
